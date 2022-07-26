a109d3ac8b6a598c11389688b6e0fbaab12ca94c
Andre Luiz <adventureandre@hotmail.com.br>
2022-07-25 22:22:13 -0300

    add notification

---
+++ src/pages/editor/renderer.js
@@ -0,0 +1,9 @@
+function notification() {
+    const NOTIFICATION_TITLE = 'Title'
+    const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.'
+    const CLICK_MESSAGE = 'Notification clicked!'
+
+    new Notification(NOTIFICATION_TITLE, {body: NOTIFICATION_BODY})
+        .onclick = () => document.getElementById("text").value = CLICK_MESSAGE
+
+}
